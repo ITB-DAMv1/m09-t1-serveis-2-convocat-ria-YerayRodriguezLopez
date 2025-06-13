b056d9550ac67fa5016e8e0b98e77f0219211afd
YerayRodriguez <yeray.rodriguez.7e5@itb.cat>
2025-06-13 13:33:34 +0200

    Database name changed

--- Events/EventAPI/obj/Debug/net8.0/EventAPI.AssemblyInfo.cs
+++ Events/EventAPI/obj/Debug/net8.0/EventAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28a6e152eb37294f2b1995d7dab370c2b369370")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d774109cd69d6ef0639ed2e847762beded2b827")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
